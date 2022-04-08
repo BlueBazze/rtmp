fc2b1ea5f10aa9d0ca630023c7fd0b4126a7516a
Tobias Jensen <bluebazze@gmail.com>
2022-04-08 10:50:31 +0200

    Adjust bitrate

--- ios/RTMPCreator.swift
+++ ios/RTMPCreator.swift
@@ -67,7 +67,7 @@ class RTMPCreator {
                 
                 .width: isLandscape ? height : width,
                 .height: isLandscape ? width : height,
-                .bitrate: 8000 * 1000,
+                .bitrate: 2000 * 1000,
                 .profileLevel: kVTProfileLevel_H264_Baseline_5_2,
                 //.profileLevel: kVTProfileLevel_H264_Baseline_AutoLevel, kVTProfileLevel_H264_Baseline_4_1 //kVTProfileLevel_H264_Baseline_3_1, kVTProfileLevel_H264_Baseline_AutoLevel
                 .maxKeyFrameIntervalDuration: 2,
